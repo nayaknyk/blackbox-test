a0a35f843ddb79e7844f3dc3d7155909b198dc14
nayaknyk <30729324+nayaknyk@users.noreply.github.com>
2019-06-14 01:58:42 +0530

    rev 1

--- functions/index.js
+++ functions/index.js
@@ -17,22 +17,35 @@ var transporter = nodemailer.createTransport({
 
 exports.sendChallan = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
-      
-        // getting dest email by query string
-        const dest = req.query.dest;
-
+        // getting parameters from query string
+        const fname = req.query.ufname;
+        const lname = req.query.ulname;
+        const dest = req.query.uemail;
+        const reg = req.query.uvid;
+        const speed = req.query.speed;
+        const location = req.query.location;
+        
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date+' '+time;
+        //email
         const mailOptions = {
             from: 'RTO Goa Test Mail<rtogoaexample@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
             to: dest,
             subject: 'Speed Violation', // email subject
-            html: `<p style="font-size: 16px;">Challan for Speed Limit Violation</p>
-                <br><br>
-                <p>Challan ID: 1002</p>
-                <p>The vehicle (GA-06-A2313) registered under your name was detected exceeding the speed limit. Please pay the fine of Rs. 100/- at your local police station/RTO office.
+            html: `<b>Challan for Speed Limit Violation</b>
+                <p>Challan ID: 001</p>
+                <p>Vehicle Reg.: `+reg+`</p>
+                <p>Registered under: `+fname+` `+lname+`</p>
+                <p>Recorded Speed: `+speed+`</p>
+                <p>Location: `+location+`</p>
+                <p>Date & Time of offence: `+dateTime+`</p>
+                <p>The vehicle registered under your name was detected exceeding the speed limit. Please pay the fine of Rs. 100/- at your local police station/RTO office.
                 <br>
                 Regards,<br>RTO Goa
-                </p><br><br>
-                <p>This is a computer generated mail</p>
+                </p><br><br><br>
+                <p>This is a computer generated mail for test purposes only and is not an official document</p>
             ` // email content in HTML
         };
   
@@ -41,7 +54,7 @@ exports.sendChallan = functions.https.onRequest((req, res) => {
             if(erro){
                 return res.send(erro.toString());
             }
-            return res.send('Email sent');
+            return res.send('Email sent for speeding');
         });
     });    
 });
